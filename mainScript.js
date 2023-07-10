e2f79ae67d49b791fa2c99d2e4ef9d541bfafdc1
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-10 20:22:39 +0200

    Finished the game functionality
    To play, refresh the page. Then, the prompt will ask for input
    five times, displaying the results of each round.

--- mainScript.js
+++ mainScript.js
@@ -1,9 +1,35 @@
-let options = Array("Rock", "Paper", "Scissors");
+let options = Array("rock", "paper", "scissors");
 
 let getComputerChoice = () => options[Math.floor(Math.random() * 3)];
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+let determineWinner = (playerSelection, computerSelection) => {
+    let userChoice = playerSelection.toLowerCase();    
+    let computerChoice = computerSelection.toLowerCase();
 
+    if (userChoice === computerChoice) {
+        return "The game is a tie!";
+    } else if (userChoice === "rock") {
+        if (computerChoice === "paper") {
+            return "You lose! Paper beats rock!";
+        } else {
+            return "You won! Rock beats scissors";
+        }
+    } else if (userChoice === "paper") {
+        if (computerChoice === "scissors") {
+            return "You lose! Scissors beats paper!";
+        } else {
+            return "You won! Paper beats rock!";
+        }
+    } else if (userChoice === "scissors") {
+        if (computerChoice === "rock") {
+            return "You lose! Rock beats scissors!";
+        } else {
+            return "You won! Scissors beats paper!";
+        }
+    } else return "Invalid input!";
+};
+
+for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Enter your choice: ");
+    console.log(determineWinner(playerSelection, getComputerChoice()));
+}

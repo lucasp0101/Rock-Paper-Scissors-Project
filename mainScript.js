bc1dad5fcf52181d2169a71725cba4a7be4339e1
Lucas Piorno <lucasp01012002@gmail.com>
2023-07-10 20:06:43 +0200

    Completed steps 1-3 from the assignment
    This includes creating the html and js files, and a function to get a
    random option from the game.

---
+++ mainScript.js
@@ -0,0 +1,9 @@
+let options = Array("Rock", "Paper", "Scissors");
+
+let getComputerChoice = () => options[Math.floor(Math.random() * 3)];
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
